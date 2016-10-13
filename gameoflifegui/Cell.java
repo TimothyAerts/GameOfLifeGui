15a37eeddb49057c67bc3e02d8f634e59b65bd5d
TimothyAerts <t.g.aerts@student.tue.nl>
2016-10-14 00:00:35 +0200

    logic included in cell update method

--- gameoflifegui/Cell.java
+++ gameoflifegui/Cell.java
@@ -35,6 +35,12 @@ public class Cell {
     void setNumNeighbours (int n){
         numNeighbours = n;
     }
+    /**
+     * 
+     * @param cell indicate the grid being used
+     * @param row row index of the current cell
+     * @param col column index of current cell
+     */
     void update (Cell[][] cell,int row, int col){
         int aliveneighbours = 0;
         int i = row - 1;
@@ -50,7 +56,17 @@ public class Cell {
             }
             i++;
     }
-       //implement rules and checks to neighbours.
+        
+       // using the previous counter the following logic sets the current cell to true or false according to the specifications 
+       if (aliveneighbours <= 2 && this.isAlive()){
+           this.setAlive(false);
+       }else if (aliveneighbours >= 3 && this.isAlive()){
+           this.setAlive(false);
+       }else if (2<= aliveneighbours && aliveneighbours <= 3 && this.isAlive()){
+           this.setAlive(true);
+       }else if (!this.isAlive() && aliveneighbours == 3){
+           this.setAlive(true);
+       }
        
     }
 }
